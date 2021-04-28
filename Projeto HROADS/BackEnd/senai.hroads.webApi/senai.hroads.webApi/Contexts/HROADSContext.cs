a6e31ce538184c3a8472a248e85ff7ac88c9d2ac
Marcos Roberto Santos Brito <48784309822@info.local>
2021-04-28 17:00:18 -0300

    Domains Adicionada

---
+++ Projeto HROADS/BackEnd/senai.hroads.webApi/senai.hroads.webApi/Contexts/HROADSContext.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using senai.hroads.webApi.Domains;
+
+#nullable disable
+
+namespace senai.hroads.webApi.Contexts
+{
+    public partial class HROADSContext : DbContext
+    {
+        public HROADSContext()
+        {
+        }
+
+        public HROADSContext(DbContextOptions<HROADSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Class> Classes { get; set; }
+        public virtual DbSet<ClassesHabilidade> ClassesHabilidades { get; set; }
+        public virtual DbSet<Habilidade> Habilidades { get; set; }
+        public virtual DbSet<Personagen> Personagens { get; set; }
+        public virtual DbSet<TiposHabilidade> TiposHabilidades { get; set; }
+        public virtual DbSet<TiposUsuario> TiposUsuarios { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=LAB08DESK601\\SQLEXPRESS; initial catalog=SENAI_HROADS_TARDE; user Id=sa; pwd=sa132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Class>(entity =>
+            {
+                entity.HasKey(e => e.IdClasse)
+                    .HasName("PK__Classes__0652AF79752BE3B6");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<ClassesHabilidade>(entity =>
+            {
+                entity.HasNoKey();
+
+                entity.HasOne(d => d.IdClasseNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdClasse)
+                    .HasConstraintName("FK__ClassesHa__IdCla__3D5E1FD2");
+
+                entity.HasOne(d => d.IdHabilidadeNavigation)
+                    .WithMany()
+                    .HasForeignKey(d => d.IdHabilidade)
+                    .HasConstraintName("FK__ClassesHa__IdHab__3E52440B");
+            });
+
+            modelBuilder.Entity<Habilidade>(entity =>
+            {
+                entity.HasKey(e => e.IdHabilidade)
+                    .HasName("PK__Habilida__0DD4B30DF7FBAE19");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTipoNavigation)
+                    .WithMany(p => p.Habilidades)
+                    .HasForeignKey(d => d.IdTipo)
+                    .HasConstraintName("FK__Habilidad__IdTip__398D8EEE");
+            });
+
+            modelBuilder.Entity<Personagen>(entity =>
+            {
+                entity.HasKey(e => e.IdPersonagens)
+                    .HasName("PK__Personag__D9AFE9CFB15C9B27");
+
+                entity.Property(e => e.DataAtualiz).HasColumnType("date");
+
+                entity.Property(e => e.DateCriacao).HasColumnType("date");
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdClasseNavigation)
+                    .WithMany(p => p.Personagens)
+                    .HasForeignKey(d => d.IdClasse)
+                    .HasConstraintName("FK__Personage__IdCla__412EB0B6");
+            });
+
+            modelBuilder.Entity<TiposHabilidade>(entity =>
+            {
+                entity.HasKey(e => e.IdTipo)
+                    .HasName("PK__TiposHab__9E3A29A51C8ECA45");
+
+                entity.Property(e => e.Tipo)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<TiposUsuario>(entity =>
+            {
+                entity.HasKey(e => e.IdTiposUsuarios)
+                    .HasName("PK__TiposUsu__31B52EC323877BA7");
+
+                entity.Property(e => e.TiposUsuarios)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsurios)
+                    .HasName("PK__Usuarios__2E7A37EDE0ED9FBC");
+
+                entity.HasIndex(e => e.Senha, "UQ__Usuarios__7ABB973120A3DB31")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(150)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.IdTiposUsuariosNavigation)
+                    .WithMany(p => p.Usuarios)
+                    .HasForeignKey(d => d.IdTiposUsuarios)
+                    .HasConstraintName("FK__Usuarios__IdTipo__4CA06362");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Projeto HROADS/BackEnd/senai.hroads.webApi/senai.hroads.webApi/Domains/Personagen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace senai.hroads.webApi.Domains
+{
+    public partial class Personagen
+    {
+        public int IdPersonagens { get; set; }
+        public int? IdClasse { get; set; }
+        public string Nome { get; set; }
+        public int CapacidadeMaxVida { get; set; }
+        public int CapacidadeMaxMana { get; set; }
+        public DateTime DataAtualiz { get; set; }
+        public DateTime DateCriacao { get; set; }
+
+        public virtual Class IdClasseNavigation { get; set; }
+    }
+}
