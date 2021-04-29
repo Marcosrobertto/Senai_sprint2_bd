83b0d0acc958f0c49b3d21a9bc38f669905d0c18
Marcos Roberto Santos Brito <48784309822@info.local>
2021-04-29 15:57:43 -0300

    Interfases Adicionada

--- Projeto HROADS/BackEnd/senai.hroads.webApi/senai.hroads.webApi/Domains/Class.cs
+++ Projeto HROADS/BackEnd/senai.hroads.webApi/senai.hroads.webApi/Domains/Class.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 #nullable disable
 
 namespace senai.hroads.webApi.Domains
+
 {
     public partial class Class
     {
---
+++ Projeto HROADS/BackEnd/senai.hroads.webApi/senai.hroads.webApi/Interfaces/ITiposUsuario.cs
@@ -0,0 +1,46 @@
+﻿using senai.hroads.webApi.Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace senai.hroads.webApi.Interfaces
+{
+    interface ITiposUsuario
+    {
+        /// <summary>
+        /// Retorna uma lista de tipos de usuários
+        /// </summary>
+        /// <returns>Uma lista de tipos de usuários</returns>
+        List<TiposUsuario> Listar();
+
+        /// <summary>
+        /// Busca um tipo de usuário pelo seu id
+        /// </summary>
+        /// <param name="id">Id do tipo de usuário que será buscado</param>
+        /// <returns>Um tipo de usuário buscada</returns>
+        TiposUsuario BuscarPorId(int id);
+
+        /// <summary>
+        /// Cadastra um tipo de usuário 
+        /// </summary>
+        /// <param name="novoTiposUsuario">Objeto novoTipoUsuario que será cadastrado</param>
+        void Cadastrar(TiposUsuario novoTiposUsuario);
+
+        /// <summary>
+        /// Atualiza um tipo de usuário existente
+        /// </summary>
+        /// <param name="id">ID dao tipo usuário que será atualizada</param>
+        /// <param name="tiposUsuarioAtualizado">Objeto tipoUsuarioAtualizado com as novas informações</param>
+        void Atualizar(int id, TiposUsuario tiposUsuarioAtualizado);
+
+        /// <summary>
+        /// Deleta um tipo de usuário existente
+        /// </summary>
+        /// <param name="id">ID do tipo usuário que será deletado</param>
+        void Deletar(int id);
+
+
+
+    }
+}
